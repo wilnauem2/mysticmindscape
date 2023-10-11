77c8d76bfc2bddd83a47aa1eafb434fc0e487aca
Marc Wilnauer <mw@werkzeug-kasten.com>
2023-10-11 15:18:24 +0100

    rename site from Foundation to Mysticmindscape

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,8 +98,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Foundation`,
-        short_name: `Foundation`,
+        name: `Mysticmindscape`,
+        short_name: `Mysticmindscape`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
